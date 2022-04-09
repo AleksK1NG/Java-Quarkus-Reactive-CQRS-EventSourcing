d33e35ee32e6f3a990d9133da94a8ed86f925e31
Alexander <alexander.bryksin@yandex.ru>
2022-04-09 10:45:25 +0300

    feature: add mongodb configuration

--- src/main/java/configuration/MongoConfiguration.java
+++ src/main/java/configuration/MongoConfiguration.java
@@ -4,6 +4,7 @@ import com.mongodb.client.model.Indexes;
 import io.quarkus.mongodb.reactive.ReactiveMongoClient;
 import io.quarkus.runtime.StartupEvent;
 import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -13,32 +14,40 @@ import javax.inject.Inject;
 @ApplicationScoped
 public class MongoConfiguration {
 
+    private final static String AGGREGATE_ID = "aggregateId";
+
     @Inject
     Logger logger;
 
     @Inject
     ReactiveMongoClient mongoClient;
 
+    @ConfigProperty(name = "mongodb.database", defaultValue = "microservices")
+    String database;
+
+    @ConfigProperty(name = "mongodb.bank-account-collection", defaultValue = "bankAccounts")
+    String bankAccountsCollection;
+
     void startup(@Observes StartupEvent event) {
-        mongoClient.getDatabase("microservices")
+        mongoClient.getDatabase(database)
                 .listCollectionNames().toUni()
                 .onFailure().invoke(Throwable::printStackTrace)
                 .chain(collections -> {
                     logger.infof("COLLECTIONS >>>>>>>> %s", collections);
-                    if (collections.contains("bankAccounts")) {
+                    if (collections.contains(bankAccountsCollection)) {
                         return Uni.createFrom().voidItem();
                     }
-                    return mongoClient.getDatabase("microservices")
-                            .createCollection("bankAccounts")
+                    return mongoClient.getDatabase(database)
+                            .createCollection(bankAccountsCollection)
                             .onFailure().invoke(Throwable::printStackTrace)
-                            .chain(v -> mongoClient.getDatabase("microservices")
-                                    .getCollection("bankAccounts")
-                                    .createIndex(Indexes.ascending("aggregateId")));
+                            .chain(v -> mongoClient.getDatabase(database)
+                                    .getCollection(bankAccountsCollection)
+                                    .createIndex(Indexes.ascending(AGGREGATE_ID)));
                 })
                 .subscribe().with(result -> logger.infof("listCollections: %s", result));
 
 
-        mongoClient.getDatabase("microservices").getCollection("bankAccounts")
+        mongoClient.getDatabase(database).getCollection(bankAccountsCollection)
                 .listIndexes().collect().asList()
                 .onFailure().invoke(Throwable::printStackTrace)
                 .subscribe().with(result -> logger.infof("bankAccounts indexes: %s", result));
